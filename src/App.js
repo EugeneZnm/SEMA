c9d288e83a312cfad3a726bf7f3861cf346d7f7a
EugeneZnm <eugenenzioki@gmail.com>
2019-01-06 14:39:23 +0300

    creation of compnents

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React, { Component } from 'react';
-import MessageList from '../components/messagelist';
-import SendMessageForm from '../components/sendmessageform';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-
-  constructor(){
-    super()
-    this.state = {
-      messages: DUMMY_DATA
-    }
-  }
+  
   render() {
     return (
       <div className="App">
-        <Title/>
-        <MessageList messages={this.state.messages}/>
-        <SendMessageForm/>
+
       </div>
     );
   }
