fc14d21f6eb1c94f81c51cd0044de5facf9b3808
EugeneZnm <eugenenzioki@gmail.com>
2019-01-07 16:46:56 +0300

    auto scrolling

--- src/components/MessageLIst.js
+++ src/components/MessageLIst.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Message from './Message';
 class MessageList extends React.Component {
+
+    // preventing auto scroll to bottom if screen height of current position on screen is less than total scroll height
+    componentWillUpdate(){
+        const node = ReactDOM.findDOMNode(this)
+        this.shouldScrollToBottom = node.scrollTop + node.clientHeight + 100 >= node.scrollHeight
+    }
+
+    // enabling auto scrolling
+    componentDidUpdate(){
+        if(this.shouldScrollToBottom){
+            const node =ReactDOM.findDOMNode(this)
+            node.scrollTop = node.scrollHeight
+        }
+    }
     render() {
         return (
             <div className="message-list">
