e92cdd4aa93dba4303ab582d7ec058392f37ce33
EugeneZnm <eugenenzioki@gmail.com>
2019-01-07 15:47:40 +0300

    addition of roomslist

--- src/App.js
+++ src/App.js
@@ -12,8 +12,13 @@ class App extends Component {
   constructor(){
     super()
     this.state = {
+
       // storing messages in state
-      messages: []
+      messages: [],
+
+      // initialising joined and joinable rooms
+      joinableRooms: [],
+      joinedRooms: []
     }
 
     this.sendMessage = this.sendMessage.bind(this)
@@ -29,7 +34,19 @@ class App extends Component {
     })
 
     chatManager.connect().then(currentUser => {
-      currentUser.subscribeToRoom({
+      this.currentUser = currentUser
+      //  accessing joinable rooms in from roomlist
+      this.currentUser.getJoinableRooms().then(joinableRooms => {
+        this.setState({
+          joinableRooms, // rooms that user hasn't joined
+          joinedRooms: this.currentUser.rooms
+
+        })
+      })
+
+      .catch(err => console.log('err on joinablerooms:', errr))
+
+      this.currentUser.subscribeToRoom({
         roomId: 25025149,
         hooks: {
 
@@ -43,6 +60,8 @@ class App extends Component {
         }
       })
     })
+    .catch(err => console.log('err on joinablerooms:', errr))
+
   }
 
   // access to current user object and calling the sendmessage method to send data to chatkit
@@ -55,7 +74,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <RoomList/>
+        <RoomList rooms={[....this.state.joinableRooms, ...this.state.joinedRooms]}/>
         <MessageList messages={this.state.messages}/>
         <SendMessageForm sendMessage={this.sendMessage}/>
         <NewsRoomForm/>
